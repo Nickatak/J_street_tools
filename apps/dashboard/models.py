c2c58f5722172012210f4c85de1bec2626023123
Nickatak <nickle87@gmail.com>
2020-01-02 03:58:29 -0800

    models.py: Add custom user manager

--- apps/dashboard/models.py
+++ apps/dashboard/models.py
@@ -1,3 +1,28 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None):
+        if username is None:
+            raise TypeError('Users must have a username.')
+
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(username=username, email=self.noramlize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        if password is none:
+            raise TypeError('Superusers must absolutely have a password.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
