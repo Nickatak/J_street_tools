5af2df35187a8a0e3c6bbb219d830e96ee75f7a4
Nickatak <nickle87@gmail.com>
2020-01-10 03:02:42 -0800

    urls.py: Change prefix for auth routes

--- jst/urls.py
+++ jst/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
