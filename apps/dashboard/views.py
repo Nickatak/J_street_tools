3d1eb17f38e3b3ee780f158d2a762a2b684b27a5
Nickatak <nickle87@gmail.com>
2020-01-12 23:40:15 -0800

    views.py: Add basic redirect route

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import User
 # Create your views here.
+def index(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
