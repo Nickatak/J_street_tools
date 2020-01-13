17b6445eede18a79b4fdd232ea095a9e7021f2b3
Nickatak <nickle87@gmail.com>
2020-01-13 03:44:19 -0800

    urls.py: Add explicit login routing

--- jst/urls.py
+++ jst/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='logreg/login.html'), name="login"),
+    path('editting/', include('django.contrib.auth.urls')),
+    #path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('apps.dashboard.urls')),
 ]
