4768e5642fcc37d2bece9235b11b86c959b4b0f2
Nickatak <nickle87@gmail.com>
2020-01-12 23:40:48 -0800

    settings.py: Rename template dir

--- jst/settings.py
+++ jst/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'jst.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'auth_templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
